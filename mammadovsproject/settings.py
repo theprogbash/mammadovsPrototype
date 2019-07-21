0f2a4c788e746ebe1fff285deeb2e3276799c285
Kamran Mmmdv <amorfrap@gmail.com>
2019-07-22 02:19:15 +0400

    Changes in settings.py for testing

--- mammadovsproject/settings.py
+++ mammadovsproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'uiyd)b!!29h&pw^v+=+xg#rkq=+vw^6(0aae#!jcduopijz4q!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
